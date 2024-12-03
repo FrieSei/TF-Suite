505e26fc5029ff4facd5ed59504e4abf56914c71
FrieSei <168580667+FrieSei@users.noreply.github.com>
2024-12-03 21:39:25 +0100

    Update calendar-instance.ts

--- lib/google/calendar-instance.ts
+++ lib/google/calendar-instance.ts
@@ -1,1 +1,72 @@
+// Import the CalendarService for interacting with Google Calendar
+import { CalendarService } from './calendar';
+
+// Initialize the CalendarService
+const calendarService = new CalendarService();
+
+/**
+ * List upcoming calendar events.
+ * Implement this function to fetch and return a list of events based on your needs.
+ * You might use the `getSurgeonAvailability` method or other Calendar API methods.
+ * @throws Error when not implemented.
+ */
+export const listUpcomingEvents = async () => {
+  // To be implemented: Fetch upcoming events logic.
+  throw new Error('Not implemented');
+};
+
+/**
+ * Create a new calendar event.
+ * This function will map to the `createAppointment` method in CalendarService.
+ * @param summary - Title of the event.
+ * @param description - Details about the event.
+ * @param startTime - Event start time (Date object).
+ * @param endTime - Event end time (Date object).
+ * @param attendees - Optional list of attendee email addresses.
+ * @throws Error when not implemented.
+ */
+export const createCalendarEvent = async (
+  summary: string,
+  description: string,
+  startTime: Date,
+  endTime: Date,
+  attendees?: string[]
+) => {
+  // To be implemented: Logic for creating a calendar event.
+  throw new Error('Not implemented');
+};
+
+/**
+ * Update an existing calendar event.
+ * This function will map to the `updateAppointment` method in CalendarService.
+ * @param eventId - ID of the event to update.
+ * @param summary - Optional new title for the event.
+ * @param description - Optional new description for the event.
+ * @param startTime - Optional new start time (Date object).
+ * @param endTime - Optional new end time (Date object).
+ * @param attendees - Optional new list of attendee email addresses.
+ * @throws Error when not implemented.
+ */
+export const updateCalendarEvent = async (
+  eventId: string,
+  summary?: string,
+  description?: string,
+  startTime?: Date,
+  endTime?: Date,
+  attendees?: string[]
+) => {
+  // To be implemented: Logic for updating a calendar event.
+  throw new Error('Not implemented');
+};
+
+/**
+ * Delete a calendar event.
+ * This function will map to the `deleteAppointment` method in CalendarService.
+ * @param eventId - ID of the event to delete.
+ * @throws Error when not implemented.
+ */
+export const deleteCalendarEvent = async (eventId: string) => {
+  // To be implemented: Logic for deleting a calendar event.
+  throw new Error('Not implemented');
+};
 
